0560ea98b3215558bde510b8c3624bc87b29e0b7
Nizhegorodcev Denis <denis.nizhegorodcev@ukr.net>
2014-09-23 13:46:19 +0300

    hees

---
+++ www/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>js-learn</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta http-equiv="content-language" content="ru" />
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	<script type="text/javascript" src="/js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="/js/main.js"></script>
+</head>
+<body>
+	<h1>This is js learn site</h1>
+</body>
+</html>
---
+++ www/js/main.js
@@ -0,0 +1,19 @@
+$(window).load(function() {
+	
+	function pow(x) {
+
+		while (x > 1) {
+			if (x % 2 != 0) {
+				x = x*3+1
+			}else {
+				x = x/2;
+			}
+			console.log(x);
+		}
+
+	};
+
+	pow(9);
+
+
+})
